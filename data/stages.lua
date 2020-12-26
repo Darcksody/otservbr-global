a11670efd5f890cf33464ee9be179127339beba2
darcksody <darcksody@hotmail.com>
2020-12-26 00:45:50 -0600

    Feat: All stars event

--- data/stages.lua
+++ data/stages.lua
@@ -6,34 +6,34 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 20,
-		multiplier = 10 --7, 14
+		multiplier = 20 --7, 14
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 9 --6, 12
+		multiplier = 18 --6, 12
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 8 --5, 10
+		multiplier = 16 --5, 10
 	}, {
 		minlevel = 61,
 		maxlevel = 80,
-		multiplier = 7 --4, 8
+		multiplier = 14 --4, 8
 	}, {
 		minlevel = 81,
 		maxlevel = 100,
-		multiplier = 6 --3, 6
+		multiplier = 12 --3, 6
 	}, {
 		minlevel = 101,
 		maxlevel = 150,
-		multiplier = 4 --2, 4
+		multiplier = 8 --2, 4
 	}, {
 		minlevel = 151,
 		maxlevel = 200,
-		multiplier = 2 --2, 4
+		multiplier = 4 --2, 4
 	}, {
 		minlevel = 201,
-		multiplier = 1 --1, 2
+		multiplier = 2 --1, 2
 	}
 }
 
@@ -41,18 +41,18 @@ skillsStages = {
 	{
 		minlevel = 10,
 		maxlevel = 20,
-		multiplier = 12
+		multiplier = 24
 	}, {
 		minlevel = 21,
 		maxlevel = 30,
-		multiplier = 9
+		multiplier = 18
 	}, {
 		minlevel = 31,
 		maxlevel = 40,
-		multiplier = 6
+		multiplier = 12
 	}, {
 		minlevel = 41,
-		multiplier = 3
+		multiplier = 6
 	}
 }
 
@@ -60,17 +60,17 @@ magicLevelStages = {
 	{
 		minlevel = 0,
 		maxlevel = 10,
-		multiplier = 10
+		multiplier = 20
 	}, {
 		minlevel = 11,
 		maxlevel = 20,
-		multiplier = 8
+		multiplier = 16
 	}, {
 		minlevel = 21,
 		maxlevel = 30,
-		multiplier = 6
+		multiplier = 12
 	}, {
 		minlevel = 31,
-		multiplier = 2
+		multiplier = 4
 	}
 }
