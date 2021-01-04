0faa3ac56da8beb3f990ffe244576254785a285f
Jonathan Rangel <44872899+JonathanRangel12@users.noreply.github.com>
2021-01-04 13:55:33 -0400

    Add files via upload

--- data/monster/elementals/ironblight.lua
+++ data/monster/elementals/ironblight.lua
@@ -80,34 +80,34 @@ monster.voices = {
 }
 
 monster.loot = {
-	{id = "gold coin", chance = 100000, maxCount = 199},
-	{id = "small emerald", chance = 10890, maxCount = 3},
-	{id = "small amethyst", chance = 15020, maxCount = 3},
-	{id = "platinum coin", chance = 100000, maxCount = 8},
+	{id = "gold coin", chance = 5000, maxCount = 199},
+	{id = "small emerald", chance = 1890, maxCount = 3},
+	{id = "small amethyst", chance = 1520, maxCount = 3},
+	{id = "platinum coin", chance = 8000, maxCount = 8},
 	{id = "red gem", chance = 3510},
 	{id = "blue gem", chance = 480},
 	{id = "epee", chance = 480},
 	{id = "magic sulphur", chance = 830},
 	{id = "sapphire hammer", chance = 480},
-	{id = "great mana potion", chance = 17640},
+	{id = "great mana potion", chance = 1760},
 	{id = "terra legs", chance = 1110},
-	{id = "ultimate health potion", chance = 18330},
+	{id = "ultimate health potion", chance = 1330},
 	{id = "composite hornbow", chance = 210},
 	{id = "springsprout rod", chance = 1720},
 	{id = "crystal of balance", chance = 4480},
 	{id = "crystal of power", chance = 2480},
-	{id = "war crystal", chance = 21230},
-	{id = "shiny stone", chance = 15230},
+	{id = "war crystal", chance = 2130},
+	{id = "shiny stone", chance = 1530},
 	{id = "jade hat", chance = 210},
 	{id = "glacial rod", chance = 960},
 	{id = "green crystal shard", chance = 5720},
-	{id = "brown crystal splinter", chance = 12610, maxCount = 2},
-	{id = "red crystal fragment", chance = 10540},
-	{id = "crystalline spikes", chance = 18400}
+	{id = "brown crystal splinter", chance = 1610, maxCount = 2},
+	{id = "red crystal fragment", chance = 1040},
+	{id = "crystalline spikes", chance = 1800}
 }
 
 monster.attacks = {
-	{name ="melee", interval = 2000, chance = 100, minDamage = 0, maxDamage = -300},
+	{name ="melee", interval = 2000, chance = 100, minDamage = -10, maxDamage = -300},
 	-- poison
 	{name ="condition", type = CONDITION_POISON, interval = 2000, chance = 10, minDamage = -460, maxDamage = -480, radius = 6, shootEffect = CONST_ANI_POISON, effect = CONST_ME_POISONAREA, target = false},
 	{name ="combat", interval = 2000, chance = 15, type = COMBAT_ICEDAMAGE, minDamage = -260, maxDamage = -350, length = 7, spread = 3, shootEffect = CONST_ANI_ICE, effect = CONST_ME_ICEATTACK, target = false},
@@ -118,7 +118,7 @@ monster.attacks = {
 monster.defenses = {
 	defense = 35,
 	armor = 35,
-	{name ="invisible", interval = 2000, chance = 20, effect = CONST_ME_MAGIC_BLUE}
+	{name ="invisible", interval = 4000, chance = 20, effect = CONST_ME_MAGIC_BLUE}
 }
 
 monster.elements = {
