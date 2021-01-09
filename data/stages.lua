da661a49f466ff24839ffdae9295715f2dda6ea6
darcksody <darcksody@hotmail.com>
2021-01-09 17:01:37 -0600

    Feat: merge with develop

--- data/stages.lua
+++ data/stages.lua
@@ -6,25 +6,29 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 20,
-		multiplier = 10 --10, 20
+		multiplier = 14 --14, 28
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 8 --8, 16
+		multiplier = 12 --12, 24
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 6 --6, 12
+		multiplier = 10 --10, 20
 	}, {
 		minlevel = 61,
 		maxlevel = 80,
-		multiplier = 4 --4, 8
+		multiplier = 8 --8, 16
 	}, {
 		minlevel = 81,
 		maxlevel = 100,
-		multiplier = 3 --3, 6
+		multiplier = 6 --6, 12
 	}, {
 		minlevel = 101,
+		maxlevel = 150,
+		multiplier = 4 --4, 8
+	}, {
+		minlevel = 151,
 		maxlevel = 200,
 		multiplier = 2 --2, 4
 	}, {
