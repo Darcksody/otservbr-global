d901b73c8adb32fd71adba7dabdd6cca73a57f8e
darcksody <darcksody@hotmail.com>
2021-01-14 16:15:18 -0600

    Fix: feiryst

--- data/scripts/actions/other/gems.lua
+++ data/scripts/actions/other/gems.lua
@@ -102,7 +102,6 @@ function gems.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	-- Gems teleport to feyrist
 	for index, value in pairs(shrine) do
 		if target.actionid == value.targetAction then
-			if (player:getStorageValue(Storage.ThreatenedDreams.TroubledMission01) == 17) then
 				if item.itemid == index then
 					player:teleportTo(value.destination)
 					player:getPosition():sendMagicEffect(value.effect)	
@@ -112,10 +111,6 @@ function gems.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 					player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
 					return true
 				end
-			else
-				player:say("When the time comes, 'small amethysts' will be accepted at this shrine.")
-				return true
-			end
 		end
 	end
 
