5b9a19a15b91174c9ca900188c9729b2c50bd64d
darcksody <darcksody@hotmail.com>
2021-02-12 01:16:57 -0600

    Feat: Doble exp

--- data/stages.lua
+++ data/stages.lua
@@ -6,22 +6,22 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 50,
-		multiplier = 100 --100, 200
+		multiplier = 200 --100, 200
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 50 --50, 100
+		multiplier = 100 --50, 100
 	}, {
 		minlevel = 101,
 		maxlevel = 150,
-		multiplier = 25 --25, 50
+		multiplier = 50 --25, 50
 	}, {
 		minlevel = 151,
 		maxlevel = 200,
-		multiplier = 2 --2 4
+		multiplier = 4 --2 4
 	}, {
 		minlevel = 201,
-		multiplier = 1 --1, 2
+		multiplier = 2 --1, 2
 	}
 }
 
@@ -29,18 +29,18 @@ skillsStages = {
 	{
 		minlevel = 10,
 		maxlevel = 20,
-		multiplier = 9 --9. 18
+		multiplier = 18 --9. 18
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 6 --6. 12
+		multiplier = 12 --6. 12
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 3 --3. 6
+		multiplier = 6 --3. 6
 	}, {
 		minlevel = 61,
-		multiplier = 1 --1. 2
+		multiplier = 2 --1. 2
 	}
 }
 
@@ -48,17 +48,17 @@ magicLevelStages = {
 	{
 		minlevel = 0,
 		maxlevel = 20,
-		multiplier = 6 --6. 12
+		multiplier = 12 --6. 12
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 4 --4. 8
+		multiplier = 8 --4. 8
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 2 --2. 4
+		multiplier = 4 --2. 4
 	}, {
 		minlevel = 61,
-		multiplier = 1 --1. 2
+		multiplier = 2 --1. 2
 	}
 }
