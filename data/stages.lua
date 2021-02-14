0f5eb5e246f6f01f6a9ab485ce6ee9e3e9004a32
darcksody <darcksody@hotmail.com>
2021-02-14 00:50:11 -0600

    Feat: New Stages

--- data/stages.lua
+++ data/stages.lua
@@ -6,22 +6,46 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 50,
-		multiplier = 200 --100, 200
+		multiplier = 200 -- 100, 200
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 100 --50, 100
+		multiplier = 100 -- 50, 100
 	}, {
 		minlevel = 101,
 		maxlevel = 150,
-		multiplier = 50 --25, 50
+		multiplier = 80 -- 40, 80
 	}, {
 		minlevel = 151,
 		maxlevel = 200,
-		multiplier = 4 --2 4
+		multiplier = 60 -- 30, 60
 	}, {
 		minlevel = 201,
-		multiplier = 2 --1, 2
+		maxlevel = 250,
+		multiplier = 40 -- 20, 40
+	}, {
+		minlevel = 251,
+		maxlevel = 300,
+		multiplier = 20 -- 10, 20
+	}, {
+		minlevel = 301,
+		maxlevel = 350,
+		multiplier = 10 -- 5, 10
+	}, {
+		minlevel = 351,
+		maxlevel = 400,
+		multiplier = 8 -- 4, 8
+	}, {
+		minlevel = 401,
+		maxlevel = 450,
+		multiplier = 6 -- 3, 6
+	}, {
+		minlevel = 451,
+		maxlevel = 500,
+		multiplier = 4 -- 2, 4
+	}, {
+		minlevel = 501,
+		multiplier = 2 -- 1, 2
 	}
 }
 
