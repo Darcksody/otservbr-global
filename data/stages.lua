e181b7b44b114be6293fe99cb07064c57b4b725c
darcksody <darcksody@hotmail.com>
2021-02-28 01:08:06 -0600

    Feat: all Stars event

--- data/stages.lua
+++ data/stages.lua
@@ -6,46 +6,46 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 50,
-		multiplier = 100 -- 100, 200
+		multiplier = 200 -- 100, 200
 	}, {
 		minlevel = 51,
 		maxlevel = 100,
-		multiplier = 50 -- 50, 100
+		multiplier = 100 -- 50, 100
 	}, {
 		minlevel = 101,
 		maxlevel = 150,
-		multiplier = 40 -- 40, 80
+		multiplier = 80 -- 40, 80
 	}, {
 		minlevel = 151,
 		maxlevel = 200,
-		multiplier = 30 -- 30, 60
+		multiplier = 60 -- 30, 60
 	}, {
 		minlevel = 201,
 		maxlevel = 250,
-		multiplier = 20 -- 20, 40
+		multiplier = 40 -- 20, 40
 	}, {
 		minlevel = 251,
 		maxlevel = 300,
-		multiplier = 10 -- 10, 20
+		multiplier = 20 -- 10, 20
 	}, {
 		minlevel = 301,
 		maxlevel = 350,
-		multiplier = 5 -- 5, 10
+		multiplier = 10 -- 5, 10
 	}, {
 		minlevel = 351,
 		maxlevel = 400,
-		multiplier = 4 -- 4, 8
+		multiplier = 8 -- 4, 8
 	}, {
 		minlevel = 401,
 		maxlevel = 450,
-		multiplier = 3 -- 3, 6
+		multiplier = 6 -- 3, 6
 	}, {
 		minlevel = 451,
 		maxlevel = 500,
-		multiplier = 2 -- 2, 4
+		multiplier = 4 -- 2, 4
 	}, {
 		minlevel = 501,
-		multiplier = 1 -- 1, 2
+		multiplier = 2 -- 1, 2
 	}
 }
 
@@ -53,18 +53,18 @@ skillsStages = {
 	{
 		minlevel = 10,
 		maxlevel = 20,
-		multiplier = 9 --9. 18
+		multiplier = 18 --9. 18
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 6 --6. 12
+		multiplier = 12 --6. 12
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 3 --3. 6
+		multiplier = 6 --3. 6
 	}, {
 		minlevel = 61,
-		multiplier = 1 --1. 2
+		multiplier = 2 --1. 2
 	}
 }
 
@@ -72,17 +72,17 @@ magicLevelStages = {
 	{
 		minlevel = 0,
 		maxlevel = 20,
-		multiplier = 6 --6. 12
+		multiplier = 12 --6. 12
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 4 --4. 8
+		multiplier = 8 --4. 8
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 2 --2. 4
+		multiplier = 4 --2. 4
 	}, {
 		minlevel = 61,
-		multiplier = 1 --1. 2
+		multiplier = 2 --1. 2
 	}
 }
