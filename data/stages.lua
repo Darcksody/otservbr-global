cd87d9f95d67abd863cd9e505b1748bf074aca23
darcksody <darcksody@hotmail.com>
2021-03-03 23:48:00 -0600

    Feat: bless 80, normal stages and vocations

--- data/stages.lua
+++ data/stages.lua
@@ -5,46 +5,6 @@
 experienceStages = {
 	{
 		minlevel = 1,
-		maxlevel = 50,
-		multiplier = 200 -- 100, 200
-	}, {
-		minlevel = 51,
-		maxlevel = 100,
-		multiplier = 100 -- 50, 100
-	}, {
-		minlevel = 101,
-		maxlevel = 150,
-		multiplier = 80 -- 40, 80
-	}, {
-		minlevel = 151,
-		maxlevel = 200,
-		multiplier = 60 -- 30, 60
-	}, {
-		minlevel = 201,
-		maxlevel = 250,
-		multiplier = 40 -- 20, 40
-	}, {
-		minlevel = 251,
-		maxlevel = 300,
-		multiplier = 20 -- 10, 20
-	}, {
-		minlevel = 301,
-		maxlevel = 350,
-		multiplier = 10 -- 5, 10
-	}, {
-		minlevel = 351,
-		maxlevel = 400,
-		multiplier = 8 -- 4, 8
-	}, {
-		minlevel = 401,
-		maxlevel = 450,
-		multiplier = 6 -- 3, 6
-	}, {
-		minlevel = 451,
-		maxlevel = 500,
-		multiplier = 4 -- 2, 4
-	}, {
-		minlevel = 501,
 		multiplier = 2 -- 1, 2
 	}
 }
@@ -53,18 +13,18 @@ skillsStages = {
 	{
 		minlevel = 10,
 		maxlevel = 20,
-		multiplier = 18 --9. 18
+		multiplier = 9 --9. 18
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 12 --6. 12
+		multiplier = 6 --6. 12
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 6 --3. 6
+		multiplier = 3 --3. 6
 	}, {
 		minlevel = 61,
-		multiplier = 2 --1. 2
+		multiplier = 1 --1. 2
 	}
 }
 
@@ -72,17 +32,17 @@ magicLevelStages = {
 	{
 		minlevel = 0,
 		maxlevel = 20,
-		multiplier = 12 --6. 12
+		multiplier = 6 --6. 12
 	}, {
 		minlevel = 21,
 		maxlevel = 40,
-		multiplier = 8 --4. 8
+		multiplier = 4 --4. 8
 	}, {
 		minlevel = 41,
 		maxlevel = 60,
-		multiplier = 4 --2. 4
+		multiplier = 2 --2. 4
 	}, {
 		minlevel = 61,
-		multiplier = 2 --1. 2
+		multiplier = 1 --1. 2
 	}
 }
