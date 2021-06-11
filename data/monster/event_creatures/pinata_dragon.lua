e071e8e2b139e8981d568ccec24bbca4260881bc
darcksody <darcksody@hotmail.com>
2021-06-11 14:28:40 -0500

    excp

--- data/monster/event_creatures/pinata_dragon.lua
+++ data/monster/event_creatures/pinata_dragon.lua
@@ -3,7 +3,7 @@ local monster = {}
 
 monster.name = "Pinata Dragon"
 monster.description = "a Pinata Dragon"
-monster.experience = 300000
+monster.experience = 1000000
 monster.outfit = {
 	lookTypeEx = 27730
 }
